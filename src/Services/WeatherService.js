28289bd5fe14b2c79b43a101ccb664c130c39e94
Sahal Sajeed <sarcasticsahal@gmail.com>
2024-08-28 12:12:51 +0500

    Done

--- src/Services/WeatherService.js
+++ src/Services/WeatherService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '0d9031d32854c42e3de5b22c6209eb6a'; 
+const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export const fetchWeatherData = async (city) => {
